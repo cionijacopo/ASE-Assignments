91011b960071b8f0a7e15357aa9f944fa4bfcd5c
cionijacopo <cionijacopo4@gmail.com>
2023-10-26 15:17:37 +0200

    Modified math_py/app.py with new routes

--- Assignment_3/math_py/app.py
+++ Assignment_3/math_py/app.py
@@ -13,7 +13,50 @@ def add():
         return make_response('Invalid input\n', 400) # HTTP 400 BAD REQUEST
 
 #add other routes here
+# Definisco la rotta
+@app.route('/sub')
+# Definisco la funzione e ciò che deve fare
+def sub():
+    a = request.args.get('a', type=float)
+    b = request.args.get('b', type=float)
+    if a and b:
+        # Serve per ottenere un oggetto di risposta
+        return make_response(jsonify(s=a-b), 200)
+    else:
+        return make_response('Invalid input\n', 400)
+    
+# Definisco la rotta
+@app.route('\mul')
+#Definisco la funzione
+def mul():
+    a = request.args.get('a', type=float)
+    b = request.args.get('b', type=float)
+    if a and b:
+        return make_response(jsonify(s=a*b), 200)
+    else:
+        return make_response('Invalid Input', 400)
 
+# Definisco la rotta
+@app.route('\div')
+#Definisco la funzione
+def div():
+    a = request.args.get('a', type=float)
+    b = request.args.get('b', type=float)
+    if a and b:
+        return make_response(jsonify(s=a/b), 200)
+    else:
+        return make_response('Invalid Input', 400)
+
+# Definisco la rotta
+@app.route('\mod')
+#Definisco la funzione
+def mod():
+    a = request.args.get('a', type=float)
+    b = request.args.get('b', type=float)
+    if a and b:
+        return make_response(jsonify(s=a%b), 200)
+    else:
+        return make_response('Invalid Input', 400)
 
 def create_app():
     return app
