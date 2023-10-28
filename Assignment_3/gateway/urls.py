ecec5f25cdc3845c209c2e01595c8bda7a43bf4d
cionijacopo <cionijacopo4@gmail.com>
2023-10-28 19:00:30 +0200

    Updated urls.py

--- Assignment_3/gateway/urls.py
+++ Assignment_3/gateway/urls.py
@@ -1,3 +1,6 @@
-GATEWAY_URL = 'http://GATEWAY_URL:' #also add gateway service port
-MATH_URL = 'http://MATH_URL:' #also add math service port
-STRING_URL = 'http://STRING_URL:' #also add string service port
+#Ho aggiunto l'url da chiamare, la porta è 5000 perchè settata nell'altro file
+GATEWAY_URL = 'http://gateway:5000' #also add gateway service port
+#Ripeto lo stesso procedimento anche qui
+MATH_URL = 'http://math-service:5000' #also add math service port
+#Ripeto lo stesso procedimento anche qui
+STRING_URL = 'http://string-service:5000' #also add string service port
